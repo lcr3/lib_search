17241704d1421f60f02e678cdc75a66a681f7796
lcr3 <llcoolryo@gmail.com>
2021-04-19 11:49:38 +0900

    Modify search error

--- lib/ui/search_book/search_book.dart
+++ lib/ui/search_book/search_book.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:lib_search_app/network/entity/book.dart';
 import 'package:lib_search_app/network/search_book_api_client.dart';
 import 'package:lib_search_app/network/search_book_repository.dart';
@@ -15,77 +16,6 @@ class SearchBookView extends StatelessWidget {
   }
 }
 
-// class BookList extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Search Book'),
-//       ),
-//       // body: _textFieldView(),
-//     );
-//   }
-
-  // Widget _textFieldView() {
-  //   final context = useContext();
-  //   return Column(
-  //     children: [
-  //       Padding(
-  //         padding:
-  //         const EdgeInsets.only(top: 16, right: 16, bottom: 0, left: 16),
-  //         child: TextField(
-  //           // controller: searchTextField,
-  //           decoration: const InputDecoration(hintText: '検索キーワードを入力してください'),
-  //           autofocus: true,
-  //           onSubmitted: (searchKeyword) => _submission(searchKeyword, context),
-  //         ),
-  //       ),
-  //       // Expanded(
-  //       //   child: _buildList(),
-  //       // ),
-  //     ],
-  //   );
-  // }
-
-  // void _submission(String searchKeyword, BuildContext context) {
-  //   print('きたよ');
-  //   context.read(searchBookViewModelProvider)
-  //       .searchBookRequest(searchKeyword);
-  // }
-// }
-
-// class BookTileList extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     final repo = SearchBookRepositoryImpl(SearchBookApiClientImpl());
-//     return ChangeNotifierProvider<SearchBookViewModel>(
-//       name: (context) => SearchBookViewModel(repo),
-//       child: Consumer<SearchBookViewModel>(
-//           builder: (context, model, child) => Scaffold(
-//               appBar: AppBar(
-//                 title: Text('Flutter Demo Home Page'),
-//               ),
-//               body: Center(
-//                 child: Column(
-//                   mainAxisAlignment: MainAxisAlignment.center,
-//                   children: <Widget>[
-//                     Text(
-//                       'You have pushed the button this many times:',
-//                     ),
-//                     // CountText(),
-//                   ],
-//                 ),
-//               ),
-//               floatingActionButton: FloatingActionButton(
-//                 onPressed: model.searchBookRequest('aaaa'),
-//                 tooltip: 'Increment',
-//                 child: Icon(Icons.add),
-//               )
-//           )
-//       )
-//     );
-//   }
-
 class BookList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -93,7 +23,8 @@ class BookList extends StatelessWidget {
     return ChangeNotifierProvider<SearchBookViewModel>(
       create: (_) => SearchBookViewModel(repo),
       child: Consumer<SearchBookViewModel>(
-          builder: (context, model, child) => Scaffold(
+          builder: (context, model, child) =>
+              Scaffold(
                 appBar: AppBar(
                   title: const Text('Search Book'),
                 ),
@@ -113,12 +44,7 @@ class BookList extends StatelessWidget {
                         ),
                       ),
                       Expanded(
-                        child: ListView.builder(
-                            itemCount: model.searchResultList.length,
-                            itemBuilder: (BuildContext context, int index) {
-                              return BookTile(
-                                  book: model.searchResultList[index]);
-                            }),
+                        child: _bookListView(model),
                       ),
                       // CountText(),
                     ],
@@ -129,83 +55,46 @@ class BookList extends StatelessWidget {
     );
   }
 
-  // Widget _buildList() {
-    // final repositoryListState =
-    // useProvider(repositoryListViewModelProvider.state);
-    // ListView.builder(itemBuilder: itemBuilder)
+  Widget _bookListView(SearchBookViewModel model) {
+    if (model.isLoading) {
+      return const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+    if (model.error != null) {
+      // TODO(error): 動的エラーメッセージに対応する
+      return _errorView('エラーが発生しました。');
+    }
+    if (model.searchResultList.isEmpty) {
+      return _emptyListView();
+    }
+    return ListView.builder(
+        itemCount: model.searchResultList.length,
+        itemBuilder: (BuildContext context, int index) {
+          return BookTile(book: model.searchResultList[index]);
+        });
+  }
 
-    // return repositoryListState.when(
-    //   data: (repositoryList) => repositoryList.isNotEmpty
-    //       ? ListView.builder(
-    //     padding: const EdgeInsets.all(16),
-    //     itemCount: repositoryList.length,
-    //     itemBuilder: (BuildContext context, int index) {
-    //       return _repositoryTile(repositoryList[index]);
-    //     },
-    //   )
-    //       : _emptyListView(),
-    //   loading: _loadingView,
-    //   error: (error, _) => _errorView(error.toString()),
-    // );
-  // }
+  Widget _errorView(String errorMessage) {
+    Fluttertoast.showToast(
+      msg: errorMessage,
+      backgroundColor: Colors.grey,
+    );
+    return Container();
+  }
+
+  Widget _emptyListView() {
+    return const Center(
+      child: Text(
+        'お探しの書籍は見つかりませんでした。',
+        style: TextStyle(
+          color: Colors.black54,
+          fontSize: 16,
+        ),
+      ),
+    );
+  }
 }
-
-  // Consumer<ClientProvider>(
-  // builder: (_, data, __) => Expanded(
-  // child: ListView.builder(
-  // itemCount: data.clients.length,
-  // itemBuilder: (context, index) {
-  // return ClientRow(data.clients[index]);
-  // },)
-
-  // Widget _buildList() {
-  //   final searchBookState = useProvider(searchBookViewModelProvider.notifier);
-  //
-  //   return searchBookState
-  // }
-//
-//
-//     return repositoryListState.when(
-//       data: (repositoryList) => repositoryList.isNotEmpty
-//           ? ListView.builder(
-//         padding: const EdgeInsets.all(16),
-//         itemCount: repositoryList.length,
-//         itemBuilder: (BuildContext context, int index) {
-//           return _repositoryTile(repositoryList[index]);
-//         },
-//       )
-//           : _emptyListView(),
-//       loading: _loadingView,
-//       error: (error, _) => _errorView(error.toString()),
-//     );
-//   }
-//
-//   Widget _loadingView() {
-//     return const Center(
-//       child: CircularProgressIndicator(),
-//     );
-//   }
-//
-//   // Widget _errorView(String errorMessage) {
-//   //   Fluttertoast.showToast(
-//   //     msg: errorMessage,
-//   //     backgroundColor: Colors.grey,
-//   //   );
-//   //   return Container();
-//   // }
-//
-//   Widget _emptyListView() {
-//     return const Center(
-//       child: Text(
-//         'Repositoryが見つかりませんでした',
-//         style: TextStyle(
-//           color: Colors.black54,
-//           fontSize: 16,
-//         ),
-//       ),
-//     );
-//   }
-//
 
 class BookTile extends StatelessWidget {
   const BookTile({required this.book}) : super();
